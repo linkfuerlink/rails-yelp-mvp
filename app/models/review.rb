6526e409044302c842c35061a50b455c6c1f13a1
Lena Link <lenalink89@gmail.com>
2020-11-11 12:40:59 +0100

    model dependency delete added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :content, presence: true, length: { minimum: 20 }
+  validates :content, presence: true, length: { minimum: 1 }
   validates :rating, inclusion: { in: (0..5) }
   validates :rating, numericality: { only_integer: true }
   belongs_to :restaurant
